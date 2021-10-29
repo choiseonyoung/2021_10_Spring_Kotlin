b296484a17cb2ecc04e70ab49555f70ac8e15c61
choiseonyoung <bviibb@naver.com>
2021-10-29 10:29:25 +0900

    -

---
+++ KSpring03_ReadBook-v2/src/main/kotlin/com/choiseonyoung/readbook/ReadBookApplication.kt
@@ -0,0 +1,11 @@
+package com.choiseonyoung.readbook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReadBookApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReadBookApplication>(*args)
+}
